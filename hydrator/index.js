12b4b011f157004fe17f7d99178187ae06bea40e
Sylvain Lasjunies <sylvain.lasjunies@weblib.eu>
2019-12-14 13:23:31 +0100

    fix timestamp

--- hydrator/index.js
+++ hydrator/index.js
@@ -1,7 +1,7 @@
 module.exports = (packet) => packet.payload.records.map(record => ({
     imei: parseInt(packet.imei),
     ...record,
-    timestamp: new Date(record.timestamp),
+    timestamp: new Date(record.timestamp * 1000),
     latitude: record.latitude / 10000000,
     longitude: record.longitude / 10000000,
     angle: record.angle / 100,
