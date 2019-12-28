c3b0b346528185b36ae04758a5ae5eb5afb84322
Sylvain Lasjunies <sylvain.lasjunies@weblib.eu>
2019-12-28 05:27:49 +0100

    fix static field access in google service

--- services/google/index.js
+++ services/google/index.js
@@ -4,7 +4,7 @@ class Google {
     static url = 'https://maps.googleapis.com/maps/api/';
     static async getAddressFromLocation(location) {
         try {
-            const response = await axios.get(`${url}geocode/json?latlng=${location[0]},${location[1]}&key=AIzaSyCxuVAmsmw5_r1iscWKdJMP1T7CHMG77Ow`);
+            const response = await axios.get(`${Google.url}geocode/json?latlng=${location[0]},${location[1]}&key=AIzaSyCxuVAmsmw5_r1iscWKdJMP1T7CHMG77Ow`);
             return response;
         } catch (e) {
             console.log("error", e)
