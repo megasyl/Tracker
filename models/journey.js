4d918086a3e75090e4da8af7f0ae14894fb5d963
Sylvain Lasjunies <sylvain.lasjunies@weblib.eu>
2019-12-29 00:52:46 +0100

    address detail in journey

--- models/journey.js
+++ models/journey.js
@@ -12,8 +12,40 @@ module.exports = mongoose.model('Journey', new mongoose.Schema({
     beginAddress: {
         type: String,
     },
+    beginAddressRoad: {
+        type: String,
+        index: true,
+    },
+    beginAddressZip: {
+        type: Number,
+        index: true,
+    },
+    beginAddressCity: {
+        type: Number,
+        index: true,
+    },
+    beginAddressCountry: {
+        type: Number,
+        index: true,
+    },
     endAddress: {
         type: String,
+    },
+    endAddressRoad: {
+        type: String,
+        index: true,
+    },
+    endAddressZip: {
+        type: Number,
+        index: true,
+    },
+    endAddressCity: {
+        type: Number,
+        index: true,
+    },
+    endAddressCountry: {
+        type: Number,
+        index: true,
     },
     beginTimestamp: {
         type: Date,
