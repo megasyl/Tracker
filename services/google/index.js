8ff80d92de60bafc668f6e3e6dcea519058816e4
Sylvain Lasjunies <sylvain.lasjunies@weblib.eu>
2019-12-28 05:06:03 +0100

    google services api

---
+++ services/google/index.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+class Google {
+    static url = 'https://maps.googleapis.com/maps/api/';
+    static async getAddressFromLocation(location) {
+        try {
+            const response = await axios.get(`${url}geocode/json?latlng=${location[0]},${location[1]}&key=AIzaSyCxuVAmsmw5_r1iscWKdJMP1T7CHMG77Ow`);
+            return response;
+        } catch (e) {
+            console.log("error", e)
+        }
+    }
+}
+
+module.exports = Google;
