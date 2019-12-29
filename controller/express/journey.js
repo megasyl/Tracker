fa5e2eef49ca2f0ce386e8dbdfa7c4d3b378091b
Sylvain Lasjunies <sylvain.lasjunies@weblib.eu>
2019-12-29 01:29:26 +0100

    TEST

--- controller/express/journey.js
+++ controller/express/journey.js
@@ -3,14 +3,14 @@ const Provider = require('../../provider/journey');
 class Journey {
     static async read(req, res, next) {
         try {
-            const { beginDate, endDate } = req.query;
+            const { beginDate, endDate, search } = req.query;
             const match = {
                 timestamp: {
                     $gte: new Date(beginDate),
                     $lte: new Date(endDate),
                 },
             };
-            const response = await Provider.find({match});
+            const response = await Provider.find({beginAddressCity: new RegExp('^'+search+'$', "i")});
             res.status(200).send(response);
         } catch (e) {
             return next(e);
