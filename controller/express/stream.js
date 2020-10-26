dd775e93830400271d2588ae036c2daba07d67ad
Sylvain Lasjunies <megasyl20@gmail.com>
2020-10-26 20:04:30 +0100

    calculating address at each broadcast

--- controller/express/stream.js
+++ controller/express/stream.js
@@ -2,8 +2,10 @@ const hydrator = require('../../hydrator/record');
 const RecordProvider = require('../../provider/mongoose/record');
 const vehicleProvider = require('../../provider/sequelize/vehicle');
 const JourneyService = require('../../services/journey');
+const GoogleServices = require('../../services/google');
 const websocketServer = require('../../server/websocket');
 const { broadcast } = require('../../utils/websocket');
+
 
 class Stream {
     static async onRecieveData(req, res, next) {
@@ -12,9 +14,11 @@ class Stream {
             await RecordProvider.bulkInsert(recordsData);
             const record = recordsData[recordsData.length - 1];
 
+            const address = await GoogleServices.getAddressFromLocation(location);
             broadcast(websocketServer, {
                 ...record,
-                vehicle: await vehicleProvider.getByImei(record.imei)
+                vehicle: await vehicleProvider.getByImei(record.imei),
+                address: address ? address.results[0]['formatted_address'] : null
             });
             await JourneyService.processRecords(recordsData);
             res.status(200).send();
